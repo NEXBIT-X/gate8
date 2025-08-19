ab383e8fbd8a26084ff650ea71667570b03662a9
ABHIJEETH-V-N <abhijeethvn2006@gmail.com>
2025-08-20 00:10:20 +0530

    Update metadata image URLs in layout.tsx
    Replaces the previous image URLs in the metadata with new links to postimg.cc. This ensures the correct images are used for social sharing and previews.

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     siteName: "G8",
     images: [
       {
-        url: "https://g8-lemon.vercel.app/twitter-image.png", // must be absolute URL
+        url: "https://postimg.cc/p5P3cgp8", // must be absolute URL
         width: 1200,
         height: 630,
         alt: "",
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "G8",
     description: "Gate Exam Mock Test Platform",
-    images: ["https://g8-lemon.vercel.app/twitter-image.png"],
+    images: ["https://postimg.cc/p5P3cgp8"],
   },
 };
 
