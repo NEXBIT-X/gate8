1e5306697687039a5106976894ae65d60c34e3a2
DevAsh <181581217+devnarayanan0@users.noreply.github.com>
2025-08-23 19:21:17 +0530

    Update logout-button.tsx

--- components/logout-button.tsx
+++ components/logout-button.tsx
@@ -13,5 +13,5 @@ export function LogoutButton() {
     router.push("/auth/login");
   };
 
-  return <Button onClick={logout}>Logout</Button>;
+  return <Button onClick={logout} className="bg-red-600 hover:bg-red-800 text-white border-red-600 hover:border-red-700">Logout</Button>;
 }
